fc0fb34322b4a93c4f07f99e0129c7fac1f24e37
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2022-05-31 22:20:34 +0200

    v4

--- encrypt/forms.py
+++ encrypt/forms.py
@@ -4,6 +4,6 @@ from django.core.validators import FileExtensionValidator
 
 class UploadForm(forms.Form):
     docfile = forms.FileField(
-        label='Dołącz plik csv',
+        label='Dołącz plik txt',
         help_text='max. 1MB',
-        validators=[FileExtensionValidator(allowed_extensions=['csv','txt' ])])
+        validators=[FileExtensionValidator(allowed_extensions=['txt' ])])
--- encrypt/models.py
+++ encrypt/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Upload(models.Model):
+    file = models.FileField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, verbose_name='User')
